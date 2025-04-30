a4fd5f8591b1d9872fd4b3f0fb716c0af52ee46b
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2025-04-30 11:23:42 +0300

    Add UserInput

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logoImage from '../assets/investment-calculator-logo.png';
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={logoImage} alt="Logo showing a money bag" />
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
